536bf708acc9e9c796e5973a8c11371d97a11f8e
sudeep-star9 <adhikarisudeep12@gmail.com>
2021-07-17 06:43:47 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let a = "apple";
+let b = "red";
+
+let c = "ball";
+let d = "round";
+
+let e = "cat";
+let f = "meow";
+
+let g = "dog";
+let h = "bone";
+
+
+let myobj={}
+
+ myobj[a]=b;
+ myobj[c]=d;
+ myobj[e]=f;
+ myobj[g]=h;
+console.log(myobj);
